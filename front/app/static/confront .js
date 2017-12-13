4ebdb32c2f536aec02bf2ce35a1226cd822999dd
joseetmanuel <joseetmanuel@gmail.com>
2017-12-13 11:32:52 -0600

    confront2

--- front/app/static/confront .js
+++
@@ -1,5 +0,0 @@
-var global_settings = {
-    urlCORS: 'http://localhost/FlotillaCartaFacturaApi',
-    uploadPath: 'D:/flotilla/Documentos/uploads/',
-    downloadPath : 'http://localhost/FlotillaCartaFacturaApi/Documentos/'
-}
